926a40b7fe0f2c365c2321d31c615f06574e4b56
Deshan Chinthana <74889481+DeshanChinthana@users.noreply.github.com>
2023-01-26 23:41:30 +0530

    update cachMiddleware

--- src/middleware/cacheMiddleware.js
+++ src/middleware/cacheMiddleware.js
@@ -1,14 +1,22 @@
 const cacheMiddleware = (store) => (next) => (action) => {
-
     if (action.type === "FETCH_WEATHER_DATA") {
-        const state = store.getState();
-        if (state.data && Date.now() - state.timestamp < 300000) { // refresh in 5min
-            return next({ type: "FETCH_WEATHER_DATA_CACHED", payload: state.data });
+
+        // Check if data is already stored in localStorage
+        const cachedData = localStorage.getItem("weatherData");
+        const cachedTimestamp = localStorage.getItem("weatherTimestamp");
+        if (cachedData && (Date.now() - cachedTimestamp < 300000)) { // refresh in 5min
+            return next({ type: "FETCH_WEATHER_DATA_CACHED", payload: JSON.parse(cachedData) });
         }
+
+        // Otherwise, continue with the fetch and store the data and timestamp in localStorage
+        return next(action).then((result) => {
+            localStorage.setItem("weatherData", JSON.stringify(result.payload));
+            localStorage.setItem("weatherTimestamp", Date.now());
+            return result;
+        });
+
     }
-
     return next(action);
-
 };
 
 export default cacheMiddleware
